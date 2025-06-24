4d60e85d3bb9e70b8f396ac50903caaeb701b1d2
anajafizadeh <anajafizadeh@ryerson.ca>
2025-06-24 10:09:25 -0400

    accounts app and bug fixes

--- articles/views.py
+++ articles/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, HttpResponse
 from . import models
 
 def articles_list(request):
-    articles = models.Article.objects.all().order_by('date')
+    articles = models.Article.objects.all().order_by('-date')
     
     args = {'articles': articles}
     return render(request, 'articles/articlelist.html', args)
 
 
 def article_detail(request, slug):
-    return HttpResponse(slug)
+    # return HttpResponse(slug)
+    article = models.Article.objects.get(slug=slug)
+    args = {'article':article}
+    return render(request, 'articles/article_detail.html', args)
